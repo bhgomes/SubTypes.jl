53f65ec345a57b9dfe3d07ccf3246b08a30b05eb
bhgomes <bhgomes.github@gmail.com>
2019-10-29 12:22:16 -0400

    setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
+<div align="center">
+
 # SubTypes.jl
-custom subtyping in Julia
+
+[![Stable Docs](https://img.shields.io/badge/docs-stable-blue.svg)](https://bhgomes.github.io/SubTypes.jl/stable)
+[![Latest Docs](https://img.shields.io/badge/docs-latest-blue.svg)](https://bhgomes.github.io/SubTypes.jl/latest)
+[![Travis Build Status](https://travis-ci.com/bhgomes/SubTypes.jl.svg?branch=master)](https://travis-ci.com/bhgomes/SubTypes.jl)
+
+_Custom subtyping in Julia_
+
+</div>
+
+## Installation
+
+To install, run the following in a Julia session:
+
+```julia
+]add SubTypes
+```
+
+---
+<div align="center">
+
+#### [(UN)LICENSE](UNLICENSE)
+Knowledge is Freedom.
+</div>
---
+++ src/SubTypes.jl
@@ -0,0 +1,97 @@
+# src/SubTypes.jl
+# Custom subtyping in Julia
+
+__precompile__(true)
+
+
+"""
+"""
+module SubTypes
+
+import Base: convert, eltype
+
+export SubType, check_predicate, predicate, flag, Constrained, ConstrainedSymbol, support
+
+
+"""
+"""
+struct SubType{T, P, Flag}
+    value::T
+    function SubType{T, P, Flag}(x::T) where {T, P, Flag}
+        check_predicate(P, Val(Flag), x)
+        return new{T, P, Flag}(x)
+    end
+end
+
+
+"""
+"""
+function check_predicate(P, _, x)
+    throw(ArgumentError("$x does not match the predicate $P"))
+end
+
+
+"""
+"""
+convert(::Type{T}, x::SubType{T}) where {T} = x.value
+
+
+"""
+"""
+eltype(::Type{SubType{T}}) where {T} = T
+
+
+"""
+"""
+eltype(x::SubType) = eltype(typeof(x))
+
+
+"""
+"""
+predicate(::Type{SubType{T, P}}) where {T, P} = P
+
+
+"""
+"""
+predicate(x::SubType) = predicate(typeof(x))
+
+
+"""
+"""
+flag(::Type{SubType{T, P, Flag}}) where {T, P, Flag} = Flag
+
+
+"""
+"""
+flag(x::SubType) = flag(typeof(x))
+
+
+"""
+"""
+const Constrained{T, S} = SubType{T, S, :Constrained}
+
+
+"""
+"""
+const ConstrainedSymbol{S} = Constrained{Symbol, S}
+
+
+"""
+"""
+function check_predicate(S, ::Val{:Constrained}, x::T) where {T}
+    if !(x in S)
+        throw("$x not in support $S")
+    end
+end
+
+
+"""
+"""
+support(::Type{Constrained{T, S}}) where {T, S} = S
+
+
+"""
+"""
+support(constrained::Constrained) = support(typeof(constrained))
+
+end  # end SubTypes
